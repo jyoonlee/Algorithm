b80e0435c55c50f1398e9c6e052ae09bbc814011
jyoonlee <2jyoons@naver.com>
2021-03-17 10:16:31 +0900

    0317 study

---
+++ python grammer/구현 방법/반복문.py
@@ -0,0 +1,21 @@
+# while문은 무한 루프 조심
+i = 1
+result = 0
+
+while i <= 9:
+    result += i
+    i += 1
+
+print(i)
+
+# for문은 인덱스를 차례차례 방문함
+# in 뒤에 오는 인자로는 리스트, 튜플, 문자열 등이 사용될 수 있음
+result = 0
+
+for i in range(1, 10):
+    result += i
+print(result)
+
+
+# 중첩된 포문은 플로이드 워셜 + 다이나믹 프로그래밍 등에서 사용된다 
+
