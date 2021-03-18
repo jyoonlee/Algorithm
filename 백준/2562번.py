069b7f9b0f106b25820fba8c5e1473d05db979d6
jyoonlee <2jyoons@naver.com>
2021-03-18 10:05:04 +0900

    0318 study

--- 백준/2562번.py
+++ 백준/2562번.py
@@ -3,4 +3,6 @@ for i in range(9):
     array.append(int(input()))
 
 print(max(array))
-print(array.index(max(array)))  # 리스트 내에서 최대값의 인덱스 위치 
+
+# 리스트 내에서 최대값의 인덱스 위치
+print(array.index(max(array))+1)
