1e9a19f99235d8fa38a820f122b9b9b62364de70
jyoonlee <2jyoons@naver.com>
2021-03-20 21:05:29 +0900

    0320 study

--- 백준/기본수학2/4948번.py
+++ 백준/기본수학2/4948번.py
@@ -1,22 +1,20 @@
 # n보다 크고 2n보다 같거나 작은 소수 개수 구하기
-import math
 
 while True:
     n = int(input())
-
-    result = [x for x in range(n+1, 2 * n + 1)]
-    if 1 in result:
-        result.remove(1)
-
-    for i in range(2, int(math.sqrt(2 * n + 1))):
-        for j in result:
-            if j % i == 0 and j != i:
-                result.remove(j)
-
-    print(len(result))
-
     if n == 0:
         break
 
+    _n = 2*n+1
 
-# 에낭그레체인가 뭔가 그거 다시써서 해보자
+    prime = [True] * (_n)
+
+    m = int(_n ** 0.5)
+
+    for i in range(2, m + 1):
+        if prime[i]:
+            for j in range(i + i, _n, i):  # 배수 다 false로 바꾸기
+                prime[j] = False
+
+    result = [x for x in range(2, _n) if prime[x] == True]
+    print(len([z for z in result if z>n]))
