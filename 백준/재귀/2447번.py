a0d96f2b647a412367afceb0bcc0a2accf17286c
jyoonlee <2jyoons@naver.com>
2021-03-21 11:39:32 +0900

    0321 study

--- 백준/재귀/2447번.py
+++ 백준/재귀/2447번.py
@@ -1,10 +1,27 @@
 # 별 찍기 문제 (실버1)
 # https://www.acmicpc.net/problem/2447
+# "".join(배열) 형태 꼭 쓰기 => 시간 절약
 
-def star(n):
-    print(n**1/3)
+import sys
+
+def star(n, m):
+    while n != 0:
+        if n % 3 == 1 and m % 3 == 1:
+            sys.stdout.write(' ')
+            return None
+
+        n = n // 3
+        m = m // 3
+    sys.stdout.write("*")
 
 
-star(int(input()))
+n = int(input())
 
-#https://daeunnniii.tistory.com/36
+for i in range(n):
+    for j in range(n):
+        star(i, j)
+    sys.stdout.write('\n')
+
+
+
+
