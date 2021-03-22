b19507d72125c46ce7f85cf9b1d29a49e3942444
jyoonlee <2jyoons@naver.com>
2021-03-22 23:49:45 +0900

    0322 study

--- 백준/9. 정렬/11650번.py
+++ 백준/9. 정렬/11650번.py
@@ -4,26 +4,23 @@ N = int(input())
 value = []
 
 for _ in range(N):
-    value.append(list(map(int, input().split())))
+    x, y = map(int, input().split())
+    value.append([y, x])
 
 value = sorted(value)
 
 for i in value:
-    print("{} {}".format(i[0], i[1]))
+    print("{} {}".format(i[1], i[0]))
 
-
-
-# 시간 초과
-# temp = []
-# for i in range(N):
-#     for j in range(i+1, N):
-#         if value[i][0] > value[j][0]:
-#             temp = value[i]
-#             value[i] = value[j]
-#             value[j] = temp
+# import sys
+# 
+# number = int(input())
+# position_list = []
 #
-#         elif value[i][0] == value[j][0]:
-#             if value[i][1] > value[j][1]:
-#                 value[i] = value[j]
-#                 value[j] = temp
-
+# for _ in range(number):
+#     position_list.append(tuple(map(int,sys.stdin.readline().split())))
+#
+# position_list.sort(key = lambda x:(x[1],x[0]))
+#
+# for item in position_list:
+#     sys.stdout.write(str(item[0])+" "+str(item[1])+"\n")
