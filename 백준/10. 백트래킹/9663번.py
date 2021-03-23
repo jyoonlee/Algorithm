182a52ee280e39f10b06685fbd5b9180fb3187e0
jyoonlee <2jyoons@naver.com>
2021-03-24 00:43:32 +0900

    0324 study

--- 백준/10. 백트래킹/9663번.py
+++ 백준/10. 백트래킹/9663번.py
@@ -1,31 +1,35 @@
 # N-queen
-
-
+# 해답법 : 행마다 하나의 퀸이 존재해야한다
+# 대각선이거나 같은 열이면 안된다.
+# 다시 풀어보기 
 
 n = int(input())
 
-check = [[0] * n for _ in range(n)]
-print(check[:][0])
-check[:][0] = [1]*n
-print(check)
-
-count = 0
+col = [0 for i in range(14)]
+ret = 0
 
 
-def nqueen(n, x, y):
-    global count
-    for i in range(x, n):
-        for j in range(y,n):
-            if check[i][j] == 0:
-                count += 1
-                check[i][:] = [1]*n
-                # check[:][i] =
-                print(check)
-                nqueen(n, x+1, y+1)
+# k번째 행에 있는 퀸이 같은 열에 있는 지 검사: col(i) = col(k)
+# k번째 행에 있는 퀸이 같은 대각선에 있는 지 검사: |col(i)-col(k)| = i-k
+
+def promising(i):
+    for k in range(i):
+        if col[i] == col[k] or abs(col[i] - col[k]) == i - k:
+            return False
+    return True
 
 
-nqueen(n, 0, 0)
-# print(count, 0, 0)
+def queens(i):
+    global ret
+
+    if i == n:
+        ret += 1
+    else:
+        for j in range(n):
+            col[i] = j
+            if promising(i):
+                queens(i+1)
 
 
-
+queens(0)
+print(ret)
