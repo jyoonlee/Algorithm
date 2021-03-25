a3257982859f9c96883028b1320163d8107e1cec
jyoonlee <2jyoons@naver.com>
2021-03-26 00:17:49 +0900

    0326 study

---
+++ 백준/기타/1541번.py
@@ -0,0 +1,16 @@
+
+T = input()
+temp = T.replace('+', ' ')
+temp = temp.replace('-', ' ')
+
+num = list(map(int, temp.split(' ')))
+op = []
+for i in list(T):
+    if i == '+':
+        op.append('+')
+    elif i == '-':
+        op.append('-')
+
+sum = 0
+if op[0]!= '+':
+    for i in range(op.index('+')):
