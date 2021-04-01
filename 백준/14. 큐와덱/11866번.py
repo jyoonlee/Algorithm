1323c579d461fdda981c6b2a0ab7b9ab728ddfb7
jyoonlee <2jyoons@naver.com>
2021-04-02 00:44:14 +0900

    0402 study

---
+++ 백준/14. 큐와덱/11866번.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+n, k = map(int, input().split())
+
+queue = deque([x for x in range(1,n+1)])
+li = []
+
+count = 1
+while len(queue) != 0:
+    if count != k:
+        queue.append(queue.popleft())
+        count += 1
+    else:
+        li.append(queue.popleft())
+        print(queue)
+        count = 0
+
+print(li)
