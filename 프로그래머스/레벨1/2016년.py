4a6c39eef63fa230c430b0bfaa2286fd856392e3
jyoonlee <2jyoons@naver.com>
2021-05-05 18:17:04 +0900

    0505 study

---
+++ 프로그래머스/레벨1/2016년.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if a == 1:
+        today = b
+    else:
+        today = sum(month[:a - 1]) + b
+
+    today %= 7
+
+    return day[today + 1]
