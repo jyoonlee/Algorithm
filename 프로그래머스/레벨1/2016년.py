a17ecb7a3cb40cb4a06d9853449477224f25a2eb
jyoonlee <2jyoons@naver.com>
2021-05-06 23:26:01 +0900

    0506 study

--- 프로그래머스/레벨1/2016년.py
+++ 프로그래머스/레벨1/2016년.py
@@ -1,13 +1,8 @@
 def solution(a, b):
     day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
-    month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    if a == 1:
-        today = b
-    else:
-        today = sum(month[:a - 1]) + b
-
+    today = sum(month[:a - 1]) + b - 1
     today %= 7
 
-    return day[today + 1]
-
+    return day[today]
