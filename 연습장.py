c5d42d9425e9d7ed59e00efb6a4619aa3161ec4c
jyoonlee <2jyoons@naver.com>
2021-05-07 21:24:44 +0900

    0507 study

--- 연습장.py
+++ 연습장.py
@@ -1,12 +1,55 @@
+import pandas as pd
 
-phone_book = ['12' ,'456' ,'789', '1012']
-phone_book.sort()
+f1 = pd.read_csv('output.csv', encoding='utf-8', header=None)
+f2 = open('results.txt', 'r', encoding='utf-8')  # MOT
 
-value = True
-for p1, p2 in zip(phone_book, phone_book[1:]):
-    if p2.startswith(p1):
-        value = False
+f1_time = f1[0].tolist()
+
+# MOT를 다 읽고, CSI는 한 줄씩 읽기
+f2_time = dict()
+
+while f2:  # MOT
+    line = f2.readline().split()
+    if len(line) != 0:
+        f2_time[line[0] + ' ' + line[1]] = line[2]
+    else:
         break
 
+label = []
+label_2 = []
+for i in f1_time:
+    fre = [0, 0]
+    fre2 = [0, 0]
+    for j in f2_time.keys():
 
-print(value)
+        if i[:10] == j[:10]:
+
+            f1_second = i.split(' ')[1].split(':')
+            f2_second = j.split(' ')[1].split(':')
+
+            if f1_second[0] == f2_second[0] and f1_second[1] == f2_second[1]:
+
+                if abs(float(f2_second[2]) - float(f1_second[2])) < 0.5:
+                    fre[int(f2_time[j])] += 1
+
+                if abs(float(f2_second[2]) - float(f1_second[2])) < 1:
+                    fre2[int(f2_time[j])] += 1
+
+    if max(fre2) == 0:
+        label.append(-1)
+        label_2.append(-1)
+    else:
+        label.append(fre.index(max(fre)))
+        label_2.append(fre2.index(max(fre2)))
+
+point = 0
+check = 0
+for i, j in zip(label, label_2):
+    if i == j:
+        point += 1
+
+    if j >= 0:
+        check += 1
+
+print(point)
+print(check)
