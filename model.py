833c576e215dd88498192f0513c68442e6f8cce9
jyoonlee <2jyoons@naver.com>
2021-05-19 23:52:02 +0900

    0519 study

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-f1 = pd.read_csv('output.csv', encoding='utf-8', header=None)
-f2 = open('results.txt', 'r', encoding='utf-8')  # MOT
+f1 = pd.read_csv('outputs.csv', encoding='utf-8', header=None)
+f2 = open('0512_results.txt', 'r', encoding='utf-8')  # MOT
 
 f1_time = f1[0].tolist()
 
@@ -25,7 +25,7 @@ for i in f1_time:
             f1_second = i.split(' ')[1].split(':')
             f2_second = j.split(' ')[1].split(':')
 
-            if int(f1_second[0]) == int(f2_second[0]) and int(f1_second[1]) == int(f2_second[1]):
+            if int(f1_second[0]) == int(f2_second[0]) and int(float(f1_second[1])) == int(float(f2_second[1])):
 
                 if abs(float(f2_second[2]) - float(f1_second[2])) < 0.5:
                     flag = 1
@@ -38,7 +38,83 @@ for i in f1_time:
     else:
         label.append(fre.index(max(fre)))
 
+# ========== 학습 모델 ========== #
+
+import pandas as pd
+# 모델 생성되면 저장하는 애(파이참 껐다 켰을 때 다시 학습안하고 기존에 시켜논 모델 불러와서 쓸 수 있음)
+import pickle
+import warnings
+
+from mlxtend.evaluate import confusion_matrix
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
+from sklearn.metrics import classification_report
+
+warnings.filterwarnings('ignore')
+
+col = list(range(0, 91))  # 채널 수 0부터 91까지(0(타임스탬프) 1-90(CSI 채널)
+df = pd.read_csv('outputs.csv', encoding='utf-8', names=col)
+
+
 print(label)
+print(len(df[0]))
+
+df.drop([df.columns[0]], axis=1, inplace=True)  # 시간 정보 필요없으니까 날려
+df['label'] = label
 
 
+X = df.drop(['label'], 1)
+y = df['label']
 
+# train 75%, valid 25%
+x_train, x_valid, y_train, y_valid = train_test_split(X, y)
+
+# PCA 차원 축소(써보고 테스트 데이터셋으로 성능 높아지는 지 선택적으로 하기)
+pca = PCA(n_components=6)  # 학습 column 90개에서 6개로 column 축소
+principalComponents = pca.fit_transform(x_train)
+nx_Train = pd.DataFrame(data=principalComponents, columns=['pc1', 'pc2', 'pc3', 'pc4', 'pc5', 'pc6'])
+print("PCA 후 분산 : ", round(sum(pca.explained_variance_ratio_), 3))
+
+# RF 파라미터
+parameter = {
+    'n_estimators': [100, 200],
+    'max_depth': [6, 8, 10, 12],
+    'min_samples_leaf': [3, 5, 7, 10],
+    'min_samples_split': [2, 3, 5, 10]}
+
+name = 'wifiSensing.asv'  # 저장할 모델 이름
+
+# ====== storage model(처음 저장할 때) ====== #
+
+# RF
+rf = RandomForestClassifier(random_state=0)
+kfold = KFold(10, shuffle=True)
+
+# 최적의 모델 생성
+rf_grid = GridSearchCV(rf, param_grid=parameter, scoring="accuracy", n_jobs=-1, cv=kfold)
+rf_grid.fit(x_train, y_train)
+
+pickle.dump(rf_grid, open(name, 'wb'))
+
+# 검증
+prediction = rf_grid.predict(x_valid)
+total_param = rf_grid.cv_results_['params']
+total_score = rf_grid.cv_results_['mean_test_score']
+
+print('best parameter: ', rf_grid.best_params_)
+print('best score: %.2f' % rf_grid.best_score_)
+print(confusion_matrix(y_valid, prediction))
+
+# ========================================== #
+
+
+# load model(저장된 거 불러와서 새로운 테스트셋 성능 검증할 때)
+load_model = pickle.load(open(name, 'rb'))
+print(len(x_valid))
+print(x_valid)
+prediction = load_model.predict(x_valid)
+
+print('score : {}'.format(round(load_model.best_estimator_.score(x_valid, y_valid), 3)))
+print(confusion_matrix(y_valid, prediction))
+print(classification_report(y_valid, prediction))
