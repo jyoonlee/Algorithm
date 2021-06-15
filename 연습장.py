96af85aeb6b669e2378d2fbcfbd8684437ba22f8
jyoonlee <2jyoons@naver.com>
2021-06-15 15:58:58 +0900

    0615 study

--- 연습장.py
+++ 연습장.py
@@ -1,34 +1,5 @@
-from collections import deque
 
+n = (201635819*2) % 2021
+print(n)
 
-def solution(progresses, speeds):
-    pro = deque(progresses)
-    speeds = deque(speeds)
-    answer = []
-    temp = []
-
-    while progresses:
-        for i in range(len(pro)):
-            print(i, temp, answer)
-            if pro[i] < 100:
-                pro[i] += speeds[i]
-
-            else:
-                if i == 0:
-                    temp.append(i)
-                    print(temp)
-                    answer.append(len(temp))
-                    temp = []
-                    pro.popleft()
-                    speeds.popleft()
-                else:
-                    temp.append(i)
-                    pro.remove(pro[i])
-                    speeds.remove(speeds[i])
-
-    return answer
-
-
-progresses = [93, 30, 55]
-speeds = [1, 30, 5]
-print(solution(progresses, speeds))
+# 201534103
